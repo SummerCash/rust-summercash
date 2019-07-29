579a4cad8f2e745417f4d4e8135adefc7a195d6c
Dowland Aiello <mitsukomegumii@gmail.com>
2019-07-29 19:30:35 -0400

    crypto, common: Offload address logic to shared hash module.

--- src/crypto/blake2.rs
+++ src/crypto/blake2.rs
@@ -1,6 +1,9 @@
 extern crate blake2; // Link blake2 hashing library
+extern crate hex; // Link hex encoding library
 
 use blake2::{Blake2s, Digest}; // Use blake2s
+
+use super::hash; // Import the hash type module
 
 /* BEGIN EXPORTED METHODS */
 
@@ -11,14 +14,14 @@ use blake2::{Blake2s, Digest}; // Use blake2s
 /// ```
 /// use summercash::crypto::blake2; // Import the blake2 hashing utility
 /// 
-/// let hash = blake2::hash_slice()
+/// let hash = blake2::hash_slice(&[1, 2, 3, 4]);
 /// ```
-pub fn hash_slice(b: &[u8]) -> &[u8] {
+pub fn hash_slice(b: &[u8]) -> hash::Hash {
     let mut hasher = Blake2s::new(); // Init hasher
     
     hasher.input(b); // Set input
 
-    return hasher.result(); // Hash input
+    return hash::new(hasher.result()[..].to_vec()); // Hash input
 }
 
 /* END EXPORTED METHODS */
@@ -30,6 +33,8 @@ mod tests {
 
     #[test]
     fn test_hash_slice() {
-        let hashed = hash_slice(b"Test"); // Hash a test message
+        let hashed = hash_slice(b"hello world"); // Hash a test message
+
+        assert_eq!(hashed.to_str(), "9aec6806794561107e594b1f6a8a6b0c92a0cba9acf5e5e93cca06f781813b0b"); // Ensure properly hashed
     }
 }
---
+++ src/crypto/mod.rs
@@ -0,0 +1,2 @@
+pub mod blake2; // Export the blake2 hashing module
+pub mod hash; // Export the hash type module
