cc14c673d0dd0dd61d07bd3e045c62a33c468442
Dowland Aiello <mitsukomegumii@gmail.com>
2019-07-28 23:51:06 -0400

    rust-summercash: Add goland prefs to .gitignore.

---
+++ src/common/fink.rs
@@ -0,0 +1,6 @@
+use num_bigint::BigUint; // Add support for large unsigned integers
+
+/// Converts a given quantity of SummerCash denoted in Finks to SMC.
+pub fn ConvertFinksToSummerCash(n_finks: BigUint) -> BigUint {
+    return 
+}
