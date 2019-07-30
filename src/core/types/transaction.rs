cb59fc13c29ef3ed858d9f01ca480e7e01baca58
Dowland Aiello <mitsukomegumii@gmail.com>
2019-07-30 10:02:27 -0400

    types: Implement transaction pseudo-constructor method.

--- src/core/types/transaction.rs
+++ src/core/types/transaction.rs
@@ -10,8 +10,18 @@ use super::logs; // Import the logs module
 
 use super::super::super::{common::address, crypto::hash}; // Import the hash & address modules
 
+/// A receipt of a transaction's execution.
+struct Receipt<'a> {
+    /// Hash of state at transaction
+    state_hash: hash::Hash,
+    /// Logs emitted at run time
+    logs: &'a [logs::Log<'a>],
+}
+
 /// A transaction between two different addresses on the SummerCash network.
 struct Transaction<'a> {
+    /// The index of the transaction in the sender's set of txs
+    nonce: u128,
     /// The sender of the transaction
     sender: address::Address,
     /// The recipient of the transaction
@@ -36,10 +46,29 @@ struct Transaction<'a> {
     genesis: bool,
 }
 
-/// A receipt of a transaction's execution.
-struct Receipt<'a> {
-    /// Hash of state at transaction
-    state_hash: hash::Hash,
-    /// Logs emitted at run time
-    logs: &'a [logs::Log<'a>],
+/* BEGIN EXPORTED METHODS */
+
+/// Implement a set of transaction helper methods.
+impl<'a> Transaction<'a> {
+    /// Initialize a new transaction instance from a given set of parameters.
+    ///
+    /// # Example TODO: Example
+    pub fn new(nonce: u128, sender: address::Address, recipient: address::Address, value_finks: BigUint, payload: &'a[u8], parents: &'a[hash::Hash]) -> Transaction<'a> {
+        Transaction{
+            nonce: nonce, // Set nonce
+            sender: sender, // Set sender
+            recipient: recipient, // Set recipient
+            value: value_finks, // Set value (in finks)
+            payload: payload, // Set payload
+            signature: None.unwrap(), // Set signature
+            parents: parents, // Set parents
+            parent_receipts: None.unwrap(), // Set parent receipts
+            timestamp: time::now_utc(), // Set timestamp
+            deployed_contract_address: None.unwrap(),
+            contract_creation: false, // Set does create contract
+            genesis: false, // Set is genesis
+        }
+    }
 }
+
+/* END EXPORTED METHODS */
