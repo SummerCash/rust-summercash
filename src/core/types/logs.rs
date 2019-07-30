bc94fff72562987273c6d9fbcdbba008dba38efe
Dowland Aiello <mitsukomegumii@gmail.com>
2019-07-30 11:12:53 -0400

    core/types: Implement log serde serialization support.

--- src/core/types/logs.rs
+++ src/core/types/logs.rs
@@ -1,7 +1,22 @@
+use serde::ser::{Serialize, SerializeStruct, Serializer}; // Import serde
+
 /// A log emitted during contract execution.
 pub struct Log<'a> {
     /// The topic of the log
-    topic: &'a str,
+    topics: &'a [&'a str],
     /// The message of the log
     message: &'a [u8],
+}
+
+/// Implement a set of serialization helper methods.
+impl<'a> Log<'a> {
+    /// Serialize the log to a byte slice.
+    pub fn serialize<S>(&self, serializer: S) -> Result<S:: Ok, S:: Error> where S: Serializer, {
+        let mut state = serializer.serialize_struct("Log", 2)?; // Serialize log
+
+        state.serialize_field("topics", &self.topics); // Serialize topics
+        state.serialize_field("message", &self.message); // Serialize message
+
+        return state.end(); // Return result
+    }
 }
