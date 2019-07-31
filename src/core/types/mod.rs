8537fb3a0334704362e7d0326d63775d2665d3c5
Dowland Aiello <mitsukomegumii@gmail.com>
2019-07-31 02:19:32 -0400

    src: Run cargo fmt.

--- src/core/types/mod.rs
+++ src/core/types/mod.rs
@@ -1,4 +1,4 @@
 pub mod logs; // Export the log types
 pub mod receipt; // Export the receipt types
-pub mod transaction; // Export the transaction type
-pub mod signature; // Export the signature type
+pub mod signature;
+pub mod transaction; // Export the transaction type // Export the signature type
