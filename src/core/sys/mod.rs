f1d1b022be54c4a8d9c9303ffb7c492121f0a067
Dowland Aiello <mitsukomegumii@gmail.com>
2019-08-09 16:27:22 -0400

    core/sys: Implement vote new() helper method.
    Signed-off-by: Dowland Aiello <mitsukomegumii@gmail.com>

--- src/core/sys/mod.rs
+++ src/core/sys/mod.rs
@@ -1,3 +1,4 @@
 pub mod config; // Export the config module
 pub mod proposal; // Export the proposal type
 pub mod system; // Export the system type
+pub mod vote; // Export the vote type
--- src/core/sys/vote.rs
+++ src/core/sys/vote.rs
@@ -1,9 +1,26 @@
+use ed25519_dalek; // Import the edwards25519 digital signature library
+
+use super::super::super::crypto::hash; // Import the hash primitive
 use super::super::types::signature; // Import the signature primitive
 
 /// A binary, signed vote regarding a particular proposal.
 pub struct Vote {
     /// The hash of the target proposal
     pub target_proposal: hash::Hash,
+    /// Whether the voter is in favor of the particular proposal or not
+    pub in_favor: bool,
     /// The signature of the voter
     pub signature: signature::Signature,
+}
+
+/// Implement a set of voting helper methods.
+impl Vote {
+    /// Initialize and sign a new vote instance.
+    pub fn new(proposal_id: hash::Hash, in_favor: bool, signature_keypair: ed25519_dalek::Keypair) -> Vote {
+        Vote {
+            target_proposal: proposal_id, // Set proposal ID
+            in_favor: in_favor, // Set in favor of proposal
+            signature: signature::Signature{public_key: signature_keypair.public, signature: signature_keypair.sign(&*proposal_id)}, // Set signature
+        } // Return initialized vote
+    }
 }
