e21cd243f914c377ba2d370ba2879280559eac93
Dowland Aiello <mitsukomegumii@gmail.com>
2019-08-09 16:36:01 -0400

    core/sys: Include in_favor in vote signature.
    Signed-off-by: Dowland Aiello <mitsukomegumii@gmail.com>

--- src/core/sys/vote.rs
+++ src/core/sys/vote.rs
@@ -1,26 +1,36 @@
 use ed25519_dalek; // Import the edwards25519 digital signature library
 
+use bincode; // Import serde bincode
+use serde::{Deserialize, Serialize}; // Import serde serialization
+
 use super::super::super::crypto::hash; // Import the hash primitive
 use super::super::types::signature; // Import the signature primitive
 
 /// A binary, signed vote regarding a particular proposal.
+#[derive(Serialize, Deserialize)]
 pub struct Vote {
     /// The hash of the target proposal
     pub target_proposal: hash::Hash,
     /// Whether the voter is in favor of the particular proposal or not
     pub in_favor: bool,
     /// The signature of the voter
-    pub signature: signature::Signature,
+    pub signature: Option<signature::Signature>,
 }
 
 /// Implement a set of voting helper methods.
 impl Vote {
     /// Initialize and sign a new vote instance.
     pub fn new(proposal_id: hash::Hash, in_favor: bool, signature_keypair: ed25519_dalek::Keypair) -> Vote {
-        Vote {
+        let mut vote: Vote = Vote {
             target_proposal: proposal_id, // Set proposal ID
             in_favor: in_favor, // Set in favor of proposal
-            signature: signature::Signature{public_key: signature_keypair.public, signature: signature_keypair.sign(&*proposal_id)}, // Set signature
-        } // Return initialized vote
+            signature: None, // No signature yet
+        }; // Initialize vote
+
+        if let Ok(serialized_vote) = bincode::serialize(&vote) { // Serialize vote
+            vote.signature = Some(signature::Signature{public_key: signature_keypair.public, signature: signature_keypair.sign(serialized_vote.as_slice())}); // Set signature
+        }
+
+        vote // Return initialized vote
     }
 }
