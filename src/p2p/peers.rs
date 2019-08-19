e30798ea7d6777e7cd92d9d53594aa8c64c07f52
Dowland Aiello <mitsukomegumii@gmail.com>
2019-08-19 09:56:11 -0400

    p2p: Add mainnet, testnet bootstrap peers.

--- src/p2p/peers.rs
+++ src/p2p/peers.rs
@@ -16,14 +16,41 @@ pub static LOCAL_TEST_NETWORK_NAME: &str = "olympia";
 pub fn get_network_bootstrap_peers(network_name: &str) -> Vec<Multiaddr> {
     match network_name {
         // We're trying to sync to the main SummerCash network
-        "andromeda" => vec![],
+        "andromeda" => vec![get_multiaddr("/ip4/108.41.124.60/tcp/2048")],
         // This time to the public test network
-        "vela" => vec![],
+        "vela" => vec![get_multiaddr("/ip4/108.41.124.60/tcp/4096")],
         // This time to the public, but bleeding-edge dev test network
-        "virgo" => vec![],
+        "virgo" => vec![get_multiaddr("/ip4/108.41.124.60/tcp/8192")],
         // This should be a completely local test network
         "olympia" => vec![],
         // Some custom network
         _ => vec![],
+    }
+}
+
+/* BEGIN INTERNAL METHODS */
+
+fn get_multiaddr(addr_str: &str) -> Multiaddr {
+    // Parse multiaddr
+    if let Ok(addr) = addr_str.parse() {
+        addr // Return address
+    } else {
+        Multiaddr::empty() // Nil multiaddr
+    }
+}
+
+/* END INTERNAL METHODS */
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_get_network_bootstrap_nodes() {
+        let main_network_boot_nodes: Vec<Multiaddr> =
+            get_network_bootstrap_peers(MAIN_NETWORK_NAME); // Get main network peers
+        assert_eq!(
+            *main_network_boot_nodes.get(0).unwrap(),
+            get_multiaddr("/ip4/108.41.124.60/tcp/2048")
+        ); // Should have one bootstrap node
     }
 }
