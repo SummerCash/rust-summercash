b4bcc4b6b26831fc6475c89b1dd62fe683cd861e
Dowland Aiello <mitsukomegumii@gmail.com>
2019-08-25 21:41:34 -0400

    p2p: Create message.rs.

--- src/p2p/mod.rs
+++ src/p2p/mod.rs
@@ -1,4 +1,5 @@
 pub mod client; // Export the client module
 pub mod config; // Export the config sync module
-pub mod network;
-pub mod peers; // Export the peer discovery module // Export the network module
+pub mod network; // Export the network module
+pub mod peers; // Export the peer discovery module
+pub mod message; // Export the message module
--- src/p2p/network.rs
+++ src/p2p/network.rs
@@ -1,4 +1,14 @@
-use super::super::core::sys::config; // Import the node config module
+/// The main SummerCash network name.
+pub static MAIN_NETWORK_NAME: &str = "andromeda";
+
+/// The stable public SummerCash test network name.
+pub static PUBLIC_TEST_NETWORK_NAME: &str = "vela";
+
+/// The bleeding-edge SummerCash network name.
+pub static DEV_TEST_NETWORK_NAME: &str = "virgo";
+
+/// The local SummerCash test network name.
+pub static LOCAL_TEST_NETWORK_NAME: &str = "olympia";
 
 /// A SummerCash network.
 pub enum Network {
@@ -17,32 +27,10 @@ impl Network {
     /// Get the string representation of a particular network.
     pub fn to_str(&self) -> &str {
         match *self {
-            Network::MainNetwork => "andromeda",
+            Network::MainNetwork => MAIN_NETWORK_NAME,
             Network::PublicTestNetwork => "vela",
             Network::DevTestNetwork => "virgo",
             Network::LocalTestNetwork => "olympia",
         } // Handle different networks
     }
-
-    /// Derive a p2p protocol path from a given message protocol.
-    pub fn derive_p2p_protocol_path(&self, protocol: &str) -> String {
-        format!(
-            "/smc/{}/{}/{}",
-            self.to_str(),
-            config::NODE_VERSION,
-            protocol
-        ) // Return formatted
-    }
 }
-
-/// The main SummerCash network name.
-pub static MAIN_NETWORK_NAME: &str = "andromeda";
-
-/// The stable public SummerCash test network name.
-pub static PUBLIC_TEST_NETWORK_NAME: &str = "vela";
-
-/// The bleeding-edge SummerCash network name.
-pub static DEV_TEST_NETWORK_NAME: &str = "virgo";
-
-/// The local SummerCash test network name.
-pub static LOCAL_TEST_NETWORK_NAME: &str = "olympia";
