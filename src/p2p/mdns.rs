aa0c4eddfd1698b0c92c542d4d9e2657230f2e67
Dowland Aiello <mitsukomegumii@gmail.com>
2020-02-06 13:52:16 -0500

    p2p: Make clippy happy.

--- src/p2p/mdns.rs
+++ src/p2p/mdns.rs
@@ -8,20 +8,14 @@ impl<TSubstream: AsyncRead + AsyncWrite + Send + Unpin + 'static>
 {
     /// Wait for an incoming mDNS message from a potential peer. Add them to the local registry if the connection succeeds.
     fn inject_event(&mut self, event: MdnsEvent) {
-        // Check what kind of packet the peer has sent us, and, from there, decide what we want to do with it.
-        match event {
-            MdnsEvent::Discovered(list) =>
-            // Go through each of the peers we were able to connect to, and add them to the localized node registry
-            {
-                for (peer, addr) in list {
-                    // Log the discovered peer to stdout
-                    debug!("Received mDNS 'alive' confirmation from peer: {}", peer);
+        if let MdnsEvent::Discovered(list) = event {
+            for (peer, addr) in list {
+                // Log the discovered peer to stdout
+                debug!("Received mDNS 'alive' confirmation from peer: {}", peer);
 
-                    // Register the discovered peer in the localized KAD DHT service instance
-                    self.kad_dht.add_address(&peer, addr);
-                }
+                // Register the discovered peer in the localized KAD DHT service instance
+                self.kad_dht.add_address(&peer, addr);
             }
-            _ => (),
         }
     }
 }
