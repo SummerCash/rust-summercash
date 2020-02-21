0fdbfc4004ef6b13d9340afe1c0ab6e28dc11bc6
Dowland Aiello <mitsukomegumii@gmail.com>
2020-02-21 11:28:08 -0500

    scripts: Update setup_server.sh.
    Signed-off-by: Dowland Aiello <mitsukomegumii@gmail.com>

--- scripts/setup_server.sh
+++ scripts/setup_server.sh
@@ -8,13 +8,20 @@ fi
 # Make a directory for the rust-summercash repo
 mkdir -p ~/rust/src/github.com/SummerCash && cd ~/rust/src/github.com/SummerCash
 
+# Make sure git is installed
+if [ -x "$(apt)" ]; then
+    sudo apt-get install git
+elif [ -x "$(brew)" ]; then
+    sudo brew install git
+fi
+
 # Download the rust-summercash source code
 git clone https://github.com/SummerCash/rust-summercash.git
 
 # Make sure rust is installed
 if ! [ -x "$(cargo)" ]; then
     # Install rust
-    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -y
 fi
 
 # Compile the SummerCash soure code
