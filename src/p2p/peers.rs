90c0038461da317559066fa31d823cddf084f776
Dowland Aiello <mitsukomegumii@gmail.com>
2020-03-13 19:32:57 -0400

    peers: Update peer list.
    Signed-off-by: Dowland Aiello <mitsukomegumii@gmail.com>

--- src/p2p/peers.rs
+++ src/p2p/peers.rs
@@ -3,13 +3,12 @@ use super::network; // Import the network module
 use libp2p::{Multiaddr, PeerId}; // Import the libp2p library
 
 /// Get a list of bootstrap peers for a particular network.
-/// TODO: Hard-code peer ids
 pub fn get_network_bootstrap_peers(network: network::Network) -> Vec<(PeerId, Multiaddr)> {
     match network {
         // We're trying to sync to the main SummerCash network
         network::Network::MainNetwork => vec![
             (
-                get_peer_id("QmQZJ5p27AcQk6QHPB3PuxyT6hn8RB488j67NGhcJ84Qmv"),
+                get_peer_id("QmUGGG7T5AicmMUjUTay3wT7jRPwfhrjM1e8W8i1DZXjBg"),
                 get_multiaddr("/dns4/node1.summer.cash/tcp/2048"),
             ),
             (
@@ -19,7 +18,7 @@ pub fn get_network_bootstrap_peers(network: network::Network) -> Vec<(PeerId, Mu
         ],
         // This time to the public test network
         network::Network::PublicTestNetwork => vec![(
-            get_peer_id("QmaCpDMGvV2BGHeYERUEnRQAwe3N8SzbUtfsmvsqQLuvuJ"),
+            get_peer_id("QmUGGG7T5AicmMUjUTay3wT7jRPwfhrjM1e8W8i1DZXjBg"),
             get_multiaddr("/ip4/108.41.124.60/tcp/4096"),
         )],
         // This time to the public, but bleeding-edge dev test network
@@ -36,7 +35,10 @@ pub fn get_network_bootstrap_peers(network: network::Network) -> Vec<(PeerId, Mu
 pub fn get_network_bootstrap_peer_addresses(network: network::Network) -> Vec<Multiaddr> {
     match network {
         // We're trying to sync to the main SummerCash network
-        network::Network::MainNetwork => vec![get_multiaddr("/ip4/108.41.124.60/tcp/2048")],
+        network::Network::MainNetwork => vec![
+            get_multiaddr("/dns4/node1.summer.cash/tcp/2048"),
+            get_multiaddr("/dns4/node2.summer.cash/tcp/2048"),
+        ],
         // This time to the public test network
         network::Network::PublicTestNetwork => vec![get_multiaddr("/ip4/108.41.124.60/tcp/4096")],
         // This time to the public, but bleeding-edge dev test network
@@ -79,7 +81,7 @@ mod tests {
             get_network_bootstrap_peer_addresses(network::Network::MainNetwork); // Get main network peers
         assert_eq!(
             *main_network_boot_nodes.get(0).unwrap(),
-            get_multiaddr("/ip4/108.41.124.60/tcp/2048")
+            get_multiaddr("/dns4/node1.summer.cash/tcp/2048")
         ); // Should have one bootstrap node
     }
 }
